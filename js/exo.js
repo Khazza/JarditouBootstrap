b793ad5e653111fd7fcebad06d576d799eab251b
Khazza <mahetoquet@gmail.com>
2023-04-11 09:51:55 +0200

    Jarditou Bootstrap Complet

--- js/exo.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $('.navbar-toggler').click(function() {
-    $('html, body').animate({
-      scrollTop: $('#navbarScrollBottom').offset().top
-    }, 500);
-  });
-});
